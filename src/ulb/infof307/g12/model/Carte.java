7008f5c68a416e028ea27f3baf8f26bff960d0fa
Humblet Raphaël <raphum0182@gmail.com>
2023-03-03 16:18:37 +0100

    Super commit, début des cartes

--- src/ulb/infof307/g12/model/Carte.java
+++ src/ulb/infof307/g12/model/Carte.java
@@ -1,4 +1,38 @@
 package ulb.infof307.g12.model;
 
 public class Carte {
+    public int id;
+    public String recto;
+    public String verso;
+
+    Carte(int id, String recto,  String verso){
+        this.id = id;
+        this.recto = recto;
+        this.verso = verso;
+    }
+
+    /**
+     * Function that
+     */
+    public void EditCarte(){
+        recto = ""; // A récuperer avec les input de l'interface
+        verso = "";
+    }
+
+    public void CreerCarte(String question, String reponse){
+        recto = question;
+        verso = reponse;
+    }
+
+    public String PrintCarte(){
+        return ("Question: "+ recto + " Réponse: "+ verso);
+    }
+
+    public String getRecto() {
+        return recto;
+    }
+
+    public String getVerso() {
+        return verso;
+    }
 }
---
+++ test/ulb/infof307/g12/model/CarteTest.java
@@ -0,0 +1,13 @@
+package ulb.infof307.g12.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarteTest {
+    @Test
+    public void test(){
+        assertEquals(1,1);
+    }
+
+}
