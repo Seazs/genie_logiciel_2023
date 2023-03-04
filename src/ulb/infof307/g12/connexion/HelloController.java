7446d7e6bb0573ef80a7c263463a92988b01178e
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-03-04 17:55:29 +0100

    JavaFX setup

---
+++ src/ulb/infof307/g12/connexion/HelloController.java
@@ -0,0 +1,14 @@
+package ulb.infof307.g12.connexion;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
