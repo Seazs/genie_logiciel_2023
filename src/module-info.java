af6fd7ac6a047f09640b37365fc370e95d031578
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-03-04 17:53:58 +0100

    Revert "JavaFX setup"
    This reverts commit c7a033b0100c0fdb95474cb23336f93bf56217dd.

--- src/module-info.java
+++
@@ -1,8 +0,0 @@
-module ulb.infof307.g12.connexion {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ulb.infof307.g12.connexion to javafx.fxml;
-    exports ulb.infof307.g12.connexion;
-}
