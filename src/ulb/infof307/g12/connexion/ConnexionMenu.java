e384678b6761f2275614e50fcf4f9c141bbff520
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-03-05 16:54:10 +0100

    Login and Register Logic

--- src/ulb/infof307/g12/connexion/ConnexionMenu.java
+++ src/ulb/infof307/g12/connexion/ConnexionMenu.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ConnexionMenu extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ConnexionMenu.class.getResource("connexion-menu-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Application Title");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/ulb/infof307/g12/connexion/Utilisateur.java
@@ -0,0 +1,32 @@
+package ulb.infof307.g12.connexion;
+
+//Création de la classe utilisateur
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Utilisateur {
+    private String pseudo;
+    private String mdp;
+    /**
+     * Constructeur de la classe utilisateur
+     * @param npseudo
+     * @param nmdp
+     */
+    public Utilisateur(String npseudo, String nmdp){
+        pseudo = npseudo;
+        mdp = nmdp;
+    }
+    public void setMdp(String nmdp){
+        mdp = nmdp;
+    }
+
+    public String getPseudo(){
+        return pseudo;
+    }
+    public String getMdp(){
+        return mdp;
+    }
+
+
+}
