7d608334790e95955476748f8c5ed42fc26979f4
Romain <romain.bennert@ulb.be>
2023-03-09 11:01:03 +0100

    Gestion du fichier de stockage des Cartes

--- src/main/java/ulb/infof307/g12/controller/storage/GestionnairePaquet.java
+++ src/main/java/ulb/infof307/g12/controller/storage/GestionnairePaquet.java
@@ -1,6 +1,8 @@
 package ulb.infof307.g12.controller.storage;
 
+import ulb.infof307.g12.model.Carte;
 import ulb.infof307.g12.model.Paquet;
+import ulb.infof307.g12.model.Utilisateur;
 
 import java.io.*;
 
@@ -27,7 +29,7 @@ public class GestionnairePaquet {
         writer.write(paquet.getCategorie());
 
         writer.close();
-
+        sauvegardeCartes(paquet,sauvegarde);
     }
 
     /**
@@ -45,11 +47,22 @@ public class GestionnairePaquet {
             FileReader fileReader = new FileReader(sauvegarde);
             BufferedReader reader = new BufferedReader(fileReader);
 
-            String categorie = reader.readLine();
+            String line = reader.readLine();
+            new Paquet(sauvegarde.getName().replace(".ulb",""), line);
+            new Carte bufferCarte =
+            while(line != null) {
+                line = reader.readLine();
+
+                if (!line.isBlank()) {
+                    String[] listdata = line.split("#");
+                    listeUtilisateur.add(new Utilisateur(listdata[0].strip(), listdata[1].strip()));
+                }
+            }
+
 
             reader.close();
 
-            return new Paquet(sauvegarde.getName().replace(".ulb",""), categorie);
+            return Paquet;
         }catch (IOException erreur){
             return null;
         }
@@ -59,8 +72,22 @@ public class GestionnairePaquet {
 
     }
 
-    public void sauvegardeCartes(Paquet paquet, File dossier){
+    public static void sauvegardeCartes(Paquet paquet, File sauvegarde) throws IOException {
+        if(!sauvegarde.exists())
+            sauvegarde.mkdir();
 
+        sauvegarde.setWritable(true);
+        FileWriter fileWriter = new FileWriter(sauvegarde,true);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+
+        for(int i = 0; i < paquet.cartes.length ; i++){
+            Carte carte = paquet.cartes[i];
+
+            writer.newLine();
+            writer.write(carte.getRecto() + "#" + carte.getVerso());
+        }
+
+        writer.close();
     }
 
 }
