32f1fcb337094c415f358047ee3d99bc5bf1b7f3
Lina Mahyo <lina.mahyo@ulb.be>
2023-03-12 13:53:42 +0100

    Replace ConnexionMenuController.java

--- src/main/java/ulb/infof307/g12/view/connection/ConnexionMenuController.java
+++ src/main/java/ulb/infof307/g12/view/connection/ConnexionMenuController.java
@@ -9,11 +9,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 import ulb.infof307.g12.controller.storage.GestionnaireUtilisateur;
 import ulb.infof307.g12.view.paquets.MenuPaquetController;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ConnexionMenuController {
     @FXML
@@ -64,6 +66,29 @@ public class ConnexionMenuController {
                 case USERNAME_DOES_ALREADY_EXIST ->
                         messageLabel.setText("Username does already exist. Please try another username!");
 
+            }
+        }
+    }
+    @FXML
+    protected void onChangePasswordButtonClick() throws IOException{
+        String username = usernameField.getText();
+        String oldPassword = passwordField.getText();
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Change password");
+        dialog.setHeaderText("Enter your new password:");
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()) {
+            String newPassword = result.get();
+            GestionnaireUtilisateur gestionnaire = new GestionnaireUtilisateur();
+            if (gestionnaire.modifierMotDePasse(username, newPassword, oldPassword)) {
+                messageLabel.setText("Password changed successfully.");
+            } else {
+                switch (gestionnaire.getStatus()) {
+                    case USERNAME_DOES_NOT_EXIST ->
+                            messageLabel.setText("Username does not exist. Please try again.");
+                    case WRONG_PASSWORD ->
+                            messageLabel.setText("Wrong password. Please try again.");
+                }
             }
         }
     }
