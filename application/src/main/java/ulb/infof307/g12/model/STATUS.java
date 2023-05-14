e704ac6c595eca3addac2feb9a84289665703ef3
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2023-05-14 15:20:56 +0200

    - changement de mot de passe localement et dans le serveur fonctionnelle - Rework de la vue du profil

--- application/src/main/java/ulb/infof307/g12/model/STATUS.java
+++ application/src/main/java/ulb/infof307/g12/model/STATUS.java
@@ -10,9 +10,11 @@ public enum STATUS {
     PASSWORD_IS_NOT_VALID("Le mot de passe contient des caractères interdits."),
     WRONG_PASSWORD("Le mot de passe est incorrect !"),
     FILE_NOT_LOADED("Le fichier n'existe pas !"),
-    SERVER_CONNEXION_ERROR("Erreur lors de la connexion au serveur"),
-    SERVER_CREATION_ERROR("Erreur lors de la création d'un utilisateur sur le serveur"),
-    DUPLICATE("Le paquet est déjà sur le store !")
+    SERVER_CONNEXION_ERROR("Erreur lors de la connexion au serveur."),
+    SERVER_CREATION_ERROR("Erreur lors de la création d'un utilisateur sur le serveur."),
+    DUPLICATE("Le paquet est déjà sur le store !"),
+    SERVER_CANNOT_CHANGE_PASSWORD("Erreur lors de la modification du mot de passe sur le serveur."),
+    CANNOT_CHANGE_PASSWORD_OFFLINE("Il n'est pas possible de changer son mot de passe localement. Veuillez vous connecter au serveur.")
     ;
 
     @Getter
