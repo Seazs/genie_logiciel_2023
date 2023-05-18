e02bf0372c62187faa86426db1908500c0bda435
Nicolas <nicolas.guilbaud@ulb.be>
2023-05-18 14:30:41 +0200

    Généricité login/register ConnexionViewController

--- application/src/main/java/ulb/infof307/g12/view/connexion/ConnexionViewController.java
+++ application/src/main/java/ulb/infof307/g12/view/connexion/ConnexionViewController.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import lombok.Setter;
+import org.apache.commons.lang3.function.TriFunction;
 import ulb.infof307.g12.view.listeners.UserCredentialsListener;
+
+import java.util.function.Function;
 
 public class ConnexionViewController {
     @FXML
@@ -24,11 +27,7 @@ public class ConnexionViewController {
      */
     @FXML
     protected void onConnectButtonClick(ActionEvent event) {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        boolean isOnline = onlineToggle.isSelected();
-        String result = listener.onLogin(username,password,isOnline);
-        messageLabel.setText(result);
+        getCredentialsAndOperate(listener::onLogin);
     }
 
     /**
@@ -37,11 +36,7 @@ public class ConnexionViewController {
      */
     @FXML
     public void onRegisterButtonClick(ActionEvent actionEvent) {
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        boolean isOnline = onlineToggle.isSelected();
-        String result = listener.onRegister(username,password,isOnline);
-        messageLabel.setText(result);
+        getCredentialsAndOperate(listener::onRegister);
     }
 
     /**
@@ -50,7 +45,20 @@ public class ConnexionViewController {
      */
     @FXML
     public void switchOnlineMode(ActionEvent actionEvent) {
-        String text = (onlineToggle.isSelected()) ?"hors ligne" : "en ligne" ;
+        String text = (onlineToggle.isSelected()) ? "hors ligne" : "en ligne" ;
         onlineToggle.setText("Passer en mode " + text);
+    }
+
+    /**
+     * Récupère les informations rentrées par l'utilisateur et exécute une méthode donnée en tenant compte de ces informations.
+     * Affiche ensuite le résultat de l'opération.
+     * @param function la méthode à exécuter
+     */
+    private void getCredentialsAndOperate(TriFunction<String,String,Boolean,String> function){
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        boolean isOnline = onlineToggle.isSelected();
+        String result = function.apply(username,password,isOnline);
+        messageLabel.setText(result);
     }
 }
