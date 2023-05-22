8846242b516ca263e0619a9a235e7547526fa91e
Nicolas <nicolas.guilbaud@ulb.be>
2023-05-22 22:23:20 +0200

    Livrables itération 4

--- application/src/main/java/module-info.java
+++ application/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module application {
+open module application {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
@@ -15,42 +15,28 @@ module application {
     requires java.desktop;
     requires javafx.swing;
 
-    opens ulb.infof307.g12.controller.javafx.connexion to javafx.fxml;
     exports ulb.infof307.g12.controller.javafx.connexion;
 
-    opens ulb.infof307.g12.view.exception to javafx.fxml;
     exports ulb.infof307.g12.view.exception;
 
-    opens ulb.infof307.g12.controller.javafx.exception to javafx.fxml;
     exports ulb.infof307.g12.controller.javafx.exception;
 
-    opens ulb.infof307.g12.view.profiles to javafx.fxml;
     exports ulb.infof307.g12.view.profiles;
 
-    opens ulb.infof307.g12.view.paquets to javafx.fxml;
     exports ulb.infof307.g12.view.paquets;
 
-    opens ulb.infof307.g12.view.connexion to javafx.fxml;
     exports ulb.infof307.g12.view.connexion;
 
     exports ulb.infof307.g12.controller.javafx.paquets;
-    opens ulb.infof307.g12.controller.javafx.paquets to javafx.fxml;
 
-    opens ulb.infof307.g12.model to javafx.base,com.fasterxml.jackson.databind;
-
-    opens ulb.infof307.g12.controller.javafx.store to javafx.fxml;
     exports ulb.infof307.g12.controller.javafx.store;
 
-    opens ulb.infof307.g12.view.store to javafx.fxml;
     exports ulb.infof307.g12.view.store;
 
-    opens ulb.infof307.g12.view.dto to com.fasterxml.jackson.databind;
     exports ulb.infof307.g12.view.dto;
 
-    opens ulb.infof307.g12.controller.javafx.synchronisation to javafx.fxml;
     exports ulb.infof307.g12.controller.javafx.synchronisation;
 
-    opens ulb.infof307.g12.view.synchronisation to javafx.fxml;
     exports ulb.infof307.g12.view.synchronisation;
 
 
